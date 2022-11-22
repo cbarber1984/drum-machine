e1d1e60f715906dc1cbddb62013ba0634359bd8c
Craig <97862337+cbarber1984@users.noreply.github.com>
2022-11-21 22:04:04 -0500

    progress from 11-21-22

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import "./App.css";
 
 function App() {
+  const drumSource = [
+    {
+      letter: "q",
+      sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      letter: "w",
+      sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      letter: "e",
+      sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <span class="fs-4">Drum Machine</span>
       </header>
+      <div id="drum-machine" className="container-md">
+        <div id="display">
+          {/* map over array of objects, each object has two key-value pairs, key to display & sound to play */}
+          {drumSource.map((item) => (
+            <div>
+              <button>{item.letter}</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }

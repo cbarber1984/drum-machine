33dde85c7c10379082a36e8ab1db3100e7c71c49
Craig <97862337+cbarber1984@users.noreply.github.com>
2022-11-22 23:22:50 -0500

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import { render } from "@testing-library/react";
 import "./App.css";
+import { Howl } from "howler";
+import { Component } from "react";
 
-function App() {
-  const drumSource = [
-    {
-      letter: "q",
-      sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-    },
-    {
-      letter: "w",
-      sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-    },
-    {
-      letter: "e",
-      sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-    },
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPadBank: bankOne,
+      display: "",
+    };
 
-  return (
-    <div className="App">
-      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <span class="fs-4">Drum Machine</span>
-      </header>
-      <div id="drum-machine" className="container-md">
-        <div id="display">
-          {/* map over array of objects, each object has two key-value pairs, key to display & sound to play */}
-          {drumSource.map((item) => (
-            <div>
-              <button>{item.letter}</button>
-            </div>
-          ))}
+    // Bindings
+    this.displayClipName = this.displayClipName.bind(this);
+  }
+
+  // React Lifecycle Hooks
+
+  // React Component Functions
+
+  // Update the display name
+  displayClipName(name) {
+    this.setState({
+      display: name,
+    });
+  }
+
+  // Render
+  render() {
+    return (
+      <div className="App">
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <span className="fs-4">Drum Machine</span>
+        </header>
+        <div id="drum-machine" className="container-md">
+          <PadBank
+            currentPadBank={this.state.currentPadBank}
+            updateDisplay={this.displayClipName}
+          />
+          <div id="display">
+            <p>Press the corresponding keys or click on them with the mouse</p>
+            <p>Sample name:</p>
+            <p id="sample-name">{this.state.display}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,250 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
+
+// React-Redux Drum Machine
+
+// Create Key Banks: KeyCodes, Key Triggers, Id's and sound clip urls and store each as objects in an array
+// Note, refer to MDN Network for info on key events as some have been depreciated or the syntax has changed.
+
+// Bank one provides some options for music creation.
+const bankOne = [
+  {
+    keyCode: 81, //
+    keyString: "Q",
+    id: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    keyCode: 87,
+    keyString: "W",
+    id: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    keyCode: 69,
+    keyString: "E",
+    id: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    keyCode: 65,
+    keyString: "A",
+    id: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    keyCode: 83,
+    keyString: "S",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    keyCode: 68,
+    keyString: "D",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    keyCode: 90,
+    keyString: "Z",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    keyCode: 88,
+    keyString: "X",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    keyCode: 67,
+    keyString: "C",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
+
+// Bank two gives the user some additional choice in their music creation.
+const bankTwo = [
+  {
+    keyCode: 81,
+    keyString: "Q",
+    id: "Chord-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+  },
+  {
+    keyCode: 87,
+    keyString: "W",
+    id: "Chord-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+  },
+  {
+    keyCode: 69,
+    keyString: "E",
+    id: "Chord-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+  },
+  {
+    keyCode: 65,
+    keyString: "A",
+    id: "Shaker",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+  },
+  {
+    keyCode: 83,
+    keyString: "S",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+  },
+  {
+    keyCode: 68,
+    keyString: "D",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+  },
+  {
+    keyCode: 90,
+    keyString: "Z",
+    id: "Punchy-Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+  },
+  {
+    keyCode: 88,
+    keyString: "X",
+    id: "Side-Stick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+  },
+  {
+    keyCode: 67,
+    keyString: "C",
+    id: "Snare",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+  },
+];
+
+// DrumPad Component (individual pads)
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    //Bindings
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.playSound = this.playSound.bind(this);
+  }
+
+  // React Lifecycle Hooks
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  // React Component Functions
+  handleKeyPress(e) {
+    // Handles any and all keypress events for the Drum machine
+    if (e.keyCode === this.props.keyCode) {
+      // 'keyCode' is passed into this components props via the PadBank component
+      this.playSound();
+      console.log(e.keyCode + " " + this.props.keyCode);
+    }
+  }
+  playSound(e) {
+    // Plays the required sound on key press
+    const sound = document.getElementById(this.props.keyString);
+    sound.currentTime = 0;
+    sound.play();
+    this.props.updateDisplay(this.props.clipId.replace(/-/g, " "));
+    // The javascript replace method will swap out strings,
+    // we use the regex to get all of the current string and swap it out
+  }
+
+  render() {
+    return (
+      <div id={this.props.clipId} onClick={this.playSound} className="drum-pad">
+        <audio
+          className="clip"
+          id={this.props.keyString}
+          src={this.props.clipSource}
+        ></audio>
+        {this.props.keyString}
+      </div>
+    );
+  } // End Render
+} // End Component
+
+// PadBank Component: Renders and popultates properties of all the DrumPads.
+class PadBank extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let padBank = this.props.currentPadBank.map((item, index, array) => {
+      return (
+        <DrumPad
+          key={index}
+          clipId={array[index].id}
+          clipSource={array[index].url}
+          keyCode={array[index].keyCode}
+          keyString={array[index].keyString}
+          updateDisplay={this.props.updateDisplay}
+        />
+      );
+    });
+    return <div className="padBank">{padBank}</div>;
+  }
+}
+
+// App Component
+class UpdatedApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPadBank: bankOne,
+      display: "",
+    };
+    //Bindings
+    this.displayClipName = this.displayClipName.bind(this);
+  }
+  // React Lifecycle Hooks
+
+  // React Component Functions
+
+  // Update the display name
+  displayClipName(name) {
+    this.setState({
+      display: name,
+    });
+  }
+  // Render
+  render() {
+    return (
+      <div id="drum-machine" className="inner-container">
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <span className="fs-4">Drum Machine</span>
+        </header>
+        <PadBank
+          currentPadBank={this.state.currentPadBank}
+          updateDisplay={this.displayClipName}
+        />
+        <div id="display">
+          <p>Press the corresponding keys or click on them with the mouse</p>
+          <p>Sample name:</p>
+          <p id="sample-name">{this.state.display}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UpdatedApp />
   </React.StrictMode>
 );
 
